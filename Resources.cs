a6277f8434de82bab4a6a954b58d98eb3c760b6d
Alex H <alexanderhilliard@yahoo.com>
2024-03-08 14:29:06 -0600

    Version 13.5

--- Resources.cs
+++ Resources.cs
@@ -4,4 +4,5 @@ public class Resources
 {
     string version = "Version 13";
     string additionalText = "March 8th 2024";
+    string differentialText = "We are inside the new-git-tutorial folder";
 }
